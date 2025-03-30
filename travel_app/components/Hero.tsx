1eabf0ffabfae0b8749f8e6098ee63862b183d9f
python-major <itumelengmajoro97@gmail.com>
2025-03-30 22:47:18 +0200

    final product (a complete app)

--- travel_app/components/Hero.tsx
+++ travel_app/components/Hero.tsx
@@ -7,8 +7,6 @@ const Hero = () => {
     <section className="max-container padding-container flex flex-col gap-20 py-10 pb-32 md:gap-28 lg:py-20 xl:flew-row">
       <div className="hero-map"/>
 
-      {/* LEFT */}
-
       <div className="relative z-20 flex flex-1 flex-col xl:w-1/2">
         <Image 
           src="/camp.svg"
@@ -26,7 +24,7 @@ const Hero = () => {
           <div className="flex items-center gap-2">
             {Array(5).fill(1).map((_, index) => (
               <Image 
-                src={"/star.svg"}
+                src="/star.svg"
                 key={index}
                 alt="star"
                 width={24}
@@ -38,7 +36,7 @@ const Hero = () => {
           <p className="bold-16 lg:bold-20 text-blue-70">
             198k
 
-            <span className="regular-16 lg:regular-20 ml:-1"> Excellent Reviews</span>
+            <span className="regular-16 lg:regular-20 ml-1">Excellent Reviews</span>
           </p>
         </div>
 
