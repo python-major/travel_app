6f43782279a86f868bbaf3bf2738b6284dd72c36
python-major <itumelengmajoro97@gmail.com>
2025-03-30 13:43:36 +0200

    features and hero updates

--- trave_app/app/page.tsx
+++ trave_app/app/page.tsx
@@ -1,7 +1,17 @@
+import Camp from "@/components/Camp";
+import Hero from "@/components/Hero";
+import Guide from "@/components/Guide";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp"
+
 export default function Home() {
   return (
-    <h1 className="font-bold text-teal-500 text-2xl">
-      Hello Word
-    </h1>
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Features />
+      <GetApp />
+    </>
   )
 }
